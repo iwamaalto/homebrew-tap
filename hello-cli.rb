9f4b3a0fc8569be187723094432ecbfb20f3905f
iwamaalto <68109463+iwamaalto@users.noreply.github.com>
2024-07-05 14:41:15 +0900

    Update hello-cli.rb

--- hello-cli.rb
+++ hello-cli.rb
@@ -2,7 +2,7 @@ class HelloCli < Formula
   desc "CLI tool for various tasks"
   homepage ""
   url "http://gitlab.akerun.com/study1/hello-cli/-/releases/v2.0.9/downloads/hello-cli_Darwin_arm64.tar.gz"
-  sha256 "SHA256_CHECKSUM"
+  sha256 "1b5d97d52284ed919fcbfb216498f4cb5a1f0da62a40bac25ecc9439cd0463d7"
   license "MIT"
 
   depends_on "make" => :build
